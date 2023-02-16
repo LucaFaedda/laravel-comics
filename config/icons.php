04d2f3ac831d76b986626975e36e29a97e60034d
LucaFaedda <lucakarl91@gmail.com>
2023-02-16 21:20:06 +0100

    aggiunto db icone

---
+++ config/icons.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    [
+        [
+            "nome"=> 'DIGITAL COMICS',
+            "href"=> '#',
+            "img"=>'../src/assets/img/buy-comics-digital-comics.png'
+        ],
+        [
+            "nome"=> 'DC MERCHANDASIE',
+            "href"=> '#',
+            "img"=>'../src/assets/img/buy-comics-merchandise.png'
+        ],
+        [
+
+            "nome"=> 'SUBSCRIPTION',
+            "href"=> '#',
+            "img"=>'../src/assets/img/buy-comics-subscriptions.png'
+        ],
+        [
+
+            "nome"=> 'COMIC SHOP LOCATION',
+            "href"=> '#',
+            "img"=>'../src/assets/img/buy-comics-shop-locator.png'
+        ],
+        [
+            "nome"=> 'DC POWER VISA',
+            "href"=> '#',
+            "img"=>'../src/assets/img/buy-dc-power-visa.svg'
+
+        ]
+    ],
+];
