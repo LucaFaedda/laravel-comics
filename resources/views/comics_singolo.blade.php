8f18ae6709366ff4c7dd308529b03e13944903d3
LucaFaedda <lucakarl91@gmail.com>
2023-02-18 12:14:35 +0100

    aggiunto mixin and variables

--- resources/views/comics_singolo.blade.php
+++ resources/views/comics_singolo.blade.php
@@ -3,12 +3,25 @@
     <div class="container">
         <div class="row">
             <div class="col-8">
-            
                 <h1>{{$comics_singolo['title']}}</h1>
+            <div class="row d-flex ">
+                <div class="col-8 d-flex justify-content-between .bg-success">
+                   <p> {{$comics_singolo['price']}}</p>
+                   <p>AVAIABLE</p>
+                </div>
+                <div class="col-4">
+                    <select>
+                        <option value="select">Check Availability</option>
+                    </select>
+
+                </div>
+            </div>
+
+            
             </div>
             <div class="col-4">
+                <h6>ADVERTISEMENT</h6>
                 <img src="{{ Vite::asset('resources/img/adv.jpg') }}" alt="adv">
-                <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt='logo'>
             </div>
         </div>
     </div>
